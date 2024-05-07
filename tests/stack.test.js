12322ec6d145441bedba68e0f453c59dd29d6d2b
DavidRadic <david.radic@icloud.com>
2024-05-07 20:17:35 +0200

    Denna commit är en av mina egna tester där jag har lagt till ett test i stack.test.js som fallerar om peek() tar bort det översta elementet. Jag har även ett fel i denna koden som gör att testet krashar men det ska lösas till nästa commit.

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -17,3 +17,12 @@ test('peek on stack with two or more elements returns the top element', () => {
     expect(stack.peek()).toBeDefined();
     expect(stack.peek()).toBe(42);
 });
+
+test('peek does not remove the top element', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    const topElement = stack.peek();
+    expect(stack.peek()).toBe(2); 
+    expect(stack.peek()).toBe(topElement); 
+});
